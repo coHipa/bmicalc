0626898c91593aa57b3441c9cc2df4fb4e9a45a4
Raul <dr.wermut@gmx.net>
2023-07-12 21:21:40 +0200

    Minor fixes, aesthetics, etc

--- main.cpp
+++ main.cpp
@@ -33,14 +33,11 @@ double bmi_calculation(double weight, double height, int choice)
 {
     if (choice == 1)
     {
-        double qm = height / 100;
-        double result = weight / pow(qm, 2);
-        return result;
+        return weight / pow((height / 100), 2);
     }
     else
     {
-        double result = 703 * (weight/pow(height, 2));
-        return result;
+        return 703 * (weight / pow(height, 2));
     }
 
 }
@@ -66,37 +63,46 @@ int main()
 
         switch (input_menu)
         {
-        case 1:
-            std::cout << "Enter your weight in kgs: ";
-            std::cin >> input_weight;
-            
-            std::cout << "Enter your height in cm: ";
-            std::cin >> input_height; 
-            
-            std::cout.precision(2);
-            std::cout << "Your BMI is: " << bmi_calculation(input_weight, input_height, 1) << std::endl;
-            rating(bmi_calculation(input_weight, input_height, 1));
-            break;
-        case 2:
-            std::cout << "Enter your weight in lbs: ";
-            std::cin >> input_weight;
-            
-            std::cout << "Enter your height in in: ";
-            std::cin >> input_height; 
-            
-            std::cout.precision(2);
-            std::cout << "Your BMI is: " << bmi_calculation(input_weight, input_height, 2) << std::endl;
-            rating(bmi_calculation(input_weight, input_height, 2));
-            break;
-        case 0:
-            active = false;
-            break;
-        default:
-            std::cout << "Invalid input!\n";
-            break;
+            case 1:
+            {
+                std::cout << "Enter your weight in kgs: ";
+                std::cin >> input_weight;
+                
+                std::cout << "Enter your height in cm: ";
+                std::cin >> input_height; 
+                double bmi_result = bmi_calculation(input_weight, input_height, 1);
+
+                std::cout.precision(2);
+                std::cout << "Your BMI is: " << bmi_result << std::endl;
+                rating(bmi_result);
+                break;
+            }
+            case 2:
+            {
+                std::cout << "Enter your weight in lbs: ";
+                std::cin >> input_weight;
+                
+                std::cout << "Enter your height in in: ";
+                std::cin >> input_height;  
+                double bmi_result = bmi_calculation(input_weight, input_height, 2);
+
+                std::cout.precision(2);
+                std::cout << "Your BMI is: " << bmi_result << std::endl;
+                rating(bmi_result);
+                break;
+            }
+            case 0:
+            {
+                active = false;
+                break;
+            }
+            default:
+            {
+                std::cout << "Invalid input!\n";
+                break;
+            }
         }
     } while (active);
     
-
     return 0;
 }
