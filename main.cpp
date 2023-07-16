abc7bf1bf1269113d9fd9503dbd78a9957d09fc3
coHipa <dr.wermut@gmx.de>
2023-07-17 01:28:27 +0200

    Minor changes, loop returned

--- main.cpp
+++ main.cpp
@@ -4,13 +4,6 @@
 
 #include "header.h"
 
-void saveResult(double result){
-    std::ofstream saveFile;
-    saveFile.open("savefile.txt", std::ios_base::app);
-    saveFile << result << '\n';
-    saveFile.close();
-}
-
 void clearConsole() {
     #if defined _WIN32
         system("cls");
@@ -19,6 +12,13 @@ void clearConsole() {
     #elif defined (__APPLE__)
         system("clear");
     #endif 
+}
+
+void saveResult(double result){
+    std::ofstream saveFile;
+    saveFile.open("savefile.txt", std::ios_base::app);
+    saveFile << result << '\n';
+    saveFile.close();
 }
 
 std::pair<char, int> enterGenderAge() {
@@ -38,96 +38,107 @@ std::pair<char, int> enterGenderAge() {
 int main() {
 
     std::cout << "Your BMI calculator\n"; 
-    std::cout << "1. calculate BMI in metric system\n";
-    std::cout << "2. calcutlate BMI in imperial system\n";
-    std::cout << "3. show last calculations\n"; 
+    bool programActive = true;
 
-    short userMenu = {};
-    std::cin >> userMenu;
+    do {
+        std::cout << "1. calculate BMI in metric system\n";
+        std::cout << "2. calcutlate BMI in imperial system\n";
+        std::cout << "3. show last calculations\n"; 
+        std::cout << "0. Exit\n";
 
-    double userWeight {};
-    double userHeight {};
-    std::pair<char, int> userInput = enterGenderAge();
-    
-    switch(userMenu) {
-        case 1: {
-            clearConsole();
+        short userMenu = {};
+        std::cin >> userMenu;
 
-            std::cout << "Enter your weight in kgs: ";
-            std::cin >> userWeight;
-            
-            std::cout << "Enter your height in cm: ";
-            std::cin >> userHeight; 
+        double userWeight {};
+        double userHeight {};
+        
+        switch(userMenu) {
+            case 1: {
+                clearConsole();
 
-            char userGender = userInput.first;
-            int userAge = userInput.second;
+                std::cout << "Enter your weight in kgs: ";
+                std::cin >> userWeight;
+                
+                std::cout << "Enter your height in cm: ";
+                std::cin >> userHeight; 
 
-            try {
-                if(userHeight > 0) {
-                    double bmiResult = userWeight / pow((userHeight / 100), 2);
+                std::pair<char, int> userInput = enterGenderAge();
+                char userGender = userInput.first;
+                int userAge = userInput.second;
 
-                    std::cout.precision(2);
-                    std::cout << "Your BMI is: " << bmiResult << '\n';
-                    rating(bmiResult, userAge, userGender);
+                try {
+                    if(userHeight > 0) {
+                        double bmiResult = userWeight / pow((userHeight / 100), 2);
 
-                    saveResult(bmiResult);
+                        std::cout.precision(2);
+                        std::cout << "Your BMI is: " << bmiResult << '\n';
+                        rating(bmiResult, userAge, userGender);
+
+                        saveResult(bmiResult);
+                    }
+                    else throw "You must be taller than 0 centimeters.\n\n";
                 }
-                else throw "You must be taller than 0 centimeters.\n\n";
-            }
-            catch(const char* msg) {
-                std::cerr << msg;
-            }
-            
-            break;
-        }
-        case 2: {
-            clearConsole();
-
-            std::cout << "Enter your weight in lbs: ";
-            std::cin >> userWeight;
-            
-            std::cout << "Enter your height in in: ";
-            std::cin >> userHeight;  
-
-            char userGender = userInput.first;
-            int userAge = userInput.second;
-
-            try {
-                if(userHeight > 0) {
-                    double bmiResult = 703 * (userWeight / pow(userHeight, 2));
-
-                    std::cout.precision(2);
-                    std::cout << "Your BMI is: " << bmiResult << '\n';
-                    rating(bmiResult, userAge, userGender);
-
-                    saveResult(bmiResult);
+                catch(const char* msg) {
+                    std::cerr << msg;
                 }
-                else throw "You must be taller than 0 inches.\n\n";      
-            }
-            catch(const char* msg) {
-                std::cerr << msg;
-            }
 
-            break;
-        }
-        case 3: {
-            clearConsole();
-            std::string line;
-            std::ifstream saveFile("savefile.txt");
-            if(saveFile.is_open()){
-                while(getline(saveFile,line)){
-                    std::cout << line << '\n';
+                break;
+            }
+            case 2: {
+                clearConsole();
+
+                std::cout << "Enter your weight in lbs: ";
+                std::cin >> userWeight;
+                
+                std::cout << "Enter your height in in: ";
+                std::cin >> userHeight;  
+
+                std::pair<char, int> userInput = enterGenderAge();
+                char userGender = userInput.first;
+                int userAge = userInput.second;
+
+                try {
+                    if(userHeight > 0) {
+                        double bmiResult = 703 * (userWeight / pow(userHeight, 2));
+
+                        std::cout.precision(2);
+                        std::cout << "Your BMI is: " << bmiResult << '\n';
+                        rating(bmiResult, userAge, userGender);
+
+                        saveResult(bmiResult);
+                    }
+                    else throw "You must be taller than 0 inches.\n\n";      
                 }
-            }
-            else std::cout << "Unable to open File";
+                catch(const char* msg) {
+                    std::cerr << msg;
+                }
 
-            break;
-        }
-        default: {
-            clearConsole();
-            std::cout << "Invalid input!\n";
-            break;
-        }
-    }  
+                break;
+            }
+            case 3: {
+                clearConsole();
+                std::string line;
+                std::ifstream saveFile("savefile.txt");
+                if(saveFile.is_open()){
+                    while(getline(saveFile,line)){
+                        std::cout << line << '\n';
+                        saveFile.close();
+                    }
+                }
+                else std::cout << "Unable to open File";
+
+                break;
+            }
+            case 0: {
+                programActive = false;
+                break;
+            }
+            default: {
+                clearConsole();
+                std::cout << "Invalid input!\n";
+                break;
+            }
+        }  
+    }while(programActive);
     return 0;
 }
